19805c8d92ae2487bb4354fe29b0705ffccd757d
Willy <willy0972205402@gmail.com>
2024-04-05 18:38:12 +0800

    Initial commit

---
+++ web3-wallet-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import ButtonVue from "./components/Button.vue";
+</script>
+
+<template>
+  <ButtonVue></ButtonVue>
+</template>
+
+<style lang="less" >
+  body {
+    padding:10px;
+  } 
+</style>
+    
---
+++ web3-wallet-app/src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+    
+</script>
+
+<template>
+ <div>
+    <van-space>
+    <van-button type="primary">創建錢包</van-button>
+    <van-button type="primary">導入錢包</van-button>
+    </van-space>
+</div>
+</template>
+
+<style lang="less" >
+    
+</style>
+    
